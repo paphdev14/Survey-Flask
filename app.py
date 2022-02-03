a7ef5a5b403f634fa2ddceee93e74d3f697bf6a2
Paphery St Fleur <paph1963@gmail.com>
2022-02-03 13:13:42 -0500

    Add starter base, survey and requirement files

--- app.py
+++ app.py
@@ -1,3 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
+from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "oh-so-secret"
+
+debug = DebugToolbarExtension(app)
+
+"""To store responses to the questions"""
+RESPONSES = []
+
+"""To store names of the survey users"""
+USERNAMES = {}
+
+@app.route('/')
+def survey_intro():
+    return render_template('survey.html')
